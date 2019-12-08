ceba017921af927c33be2845b01ffd184aa57269
Hyun-Kook Yeo <gunbuster@gmail.com>
2019-12-09 00:55:58 +0900

    Enable REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER for LCD display

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1869,7 +1869,7 @@
 // RepRapDiscount FULL GRAPHIC Smart Controller
 // http://reprap.org/wiki/RepRapDiscount_Full_Graphic_Smart_Controller
 //
-//#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
+#define REPRAP_DISCOUNT_FULL_GRAPHIC_SMART_CONTROLLER
 
 //
 // ReprapWorld Graphical LCD
